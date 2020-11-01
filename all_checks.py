1caed071138fa05339290b29dc00467644ba4f11
nkirkpatrick <norbert_kirkpatrick@yahoo.com>
2020-11-01 09:20:37 -0800

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -30,11 +30,16 @@ def main():
         (check_root_full, "Root partition full"),
     ]
 
+    everything_ok = True
+
     for check, msg in checks:
         if check():
-        print(msg)
-        sys.exit(1)
+            print(msg)
+            everything_ok= False
 
+    if not everything_ok:
+        sys.exit(1)
+    
     print("Everything ok.")
     sys.exit(0)
 
